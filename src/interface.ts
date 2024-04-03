8526fe81b8ab239d14f033918571cdf7cbead028
Mara Caoile <mara.caoile@gmail.com>
2024-04-02 20:38:19 -0500

    notes on type alias and enum values

--- src/interface.ts
+++ src/interface.ts
@@ -1,8 +1,9 @@
 interface Contact extends Address {
   id: number;
-  name: string;
+  name: ContactName;
   birthDate?: Date;
-}
+  status: ContactStatus
+;}
 
 interface Address {
   line1?: string;
@@ -12,11 +13,24 @@ interface Address {
   postalCode?: number;
 }
 
+enum ContactStatus {
+  Active = "active",
+  Inactive = "inactive",
+  New ="new"
+}
+
+// by defining enum values, you can assign a string value to the enum member below that's not their index value//
+
 let primaryContact: Contact = {
   id: 12345,
   name: "Jamie",
   postalCode: 37076,
+  status: ContactStatus.Active
 }
+
+//type alias://
+type ContactName = string
+
 
 // interface - strictly used to check types in TS and only used during compile time - it checkes for structure //
 // classes - can be instantiated to create objects - it can implement behavior and data
